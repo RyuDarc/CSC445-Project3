cbe7c525b807850d7817484cb34a116e3358ceaa
RyuDarc <ryu@oswego.edu>
2022-04-27 23:35:45 -0400

    remove uncessary file

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.csc445.jrvv.pthree {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.csc445.jrvv.pthree to javafx.fxml;
-    exports com.csc445.jrvv.pthree;
-}
